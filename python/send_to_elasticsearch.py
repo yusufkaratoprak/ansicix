5a699fbc8172ef6e2e6a05e6bf37e4ac21b640e2
yusuf karatoprak <yusufkaratoprak@yahoo.com>
2023-08-15 15:32:21 +0300

    Update send_to_elasticsearch.py

--- python/send_to_elasticsearch.py
+++ python/send_to_elasticsearch.py
@@ -1,1 +1,15 @@
+import requests
+import sys
+
+def send_to_elasticsearch(status):
+    data = {
+        "status": status
+    }
+    response = requests.post("http://YOUR_ELASTICSEARCH_URL:9200/k8s_health_status/_doc", json=data)
+    if response.status_code != 201:
+        print("Error sending data to Elasticsearch:", response.text)
+
+if __name__ == "__main__":
+    status = sys.argv[1]
+    send_to_elasticsearch(status)
 
