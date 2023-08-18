4a8e47a30315196f87668561804ba0bc887ddf3d
yusuf karatoprak <yusufkaratoprak@yahoo.com>
2023-08-19 00:51:49 +0300

    Update ansicix.go

--- go/ansicix.go
+++ go/ansicix.go
@@ -1,15 +1,17 @@
+package main
+
 import (
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	var path string
 	var podName string
+	var minute string
+	const path = "/path/to/your/ansible/playbooks"  // Sabit path değeri
 
 	var rootCmd = &cobra.Command{
 		Use:   "ansicix",
@@ -22,15 +24,17 @@ func main() {
 
 	var setupCmd = &cobra.Command{
 		Use:   "setup",
-		Short: "Run Ansible playbooks in the provided directory",
+		Short: "Run Ansible playbooks from the specified directory",
 		Run: func(cmd *cobra.Command, args []string) {
-			runAnsiblePlaybooks(path, podName)
+			runAnsiblePlaybooks(path, podName, minute)
 		},
 	}
 
-	setupCmd.Flags().StringVarP(&path, "path", "p", "", "Path to directory containing Ansible playbooks")
-	setupCmd.Flags().StringVarP(&podName, "pod", "", "", "Pod name for the Kubernetes task")
-	setupCmd.MarkFlagRequired("path")
+	// Pod ismi için flag tanımlama
+	setupCmd.Flags().StringVarP(&podName, "pod", "p", "", "Pod name for the Kubernetes task")
+	// Dakika aralığı için flag tanımlama
+	setupCmd.Flags().StringVarP(&minute, "minute", "m", "1", "Minute interval for the cron job")
+	setupCmd.MarkFlagRequired("pod")
 
 	rootCmd.AddCommand(setupCmd)
 	if err := rootCmd.Execute(); err != nil {
@@ -38,9 +42,8 @@ func main() {
 	}
 }
 
-func runAnsiblePlaybooks(path, podName string) {
-	cmd := exec.Command("ansible-playbook", "-i", "inventory", fmt.Sprintf("%s/playbook.yml", path))
-	cmd.Env = append(os.Environ(), fmt.Sprintf("POD_NAME=%s", podName))
+func runAnsiblePlaybooks(path, podName, minute string) {
+	cmd := exec.Command("ansible-playbook", "-i", "inventory", fmt.Sprintf("%s/main.yml", path), "-e", fmt.Sprintf("POD_NAME=%s minute=%s", podName, minute))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
